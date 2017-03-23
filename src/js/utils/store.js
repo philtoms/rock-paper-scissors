416b32c84cce3419773d57e5794705627b5fba35
Phil Toms <phil.toms@hotmail.co.uk>
2017-03-23 09:15:24 +0000

    Implement game engine and show results

--- src/js/utils/store.js
+++ src/js/utils/store.js
@@ -10,22 +10,20 @@ const _dispatch = action => {
     const nextState = _reduce(_state, action);
     if (nextState !== _state) {
         _state = nextState;
-        _subscribers.forEach(cb => cb(_state, _dispatch));
+        _subscribers.forEach(cb => cb(_state, _dispatch, _getState));
     }
 }
+
+const _getState = () => _state;
+
 export default class Store {
     constructor(state, reducer) {
         // No fancy map / reduce (yet)..
         _reduce = reducer;
         _state = _reduce({...state}, {type: 'store/INIT'});
-    }
 
-    dispatch(action) {
-        return _dispatch(action);
-    }
-
-    getState() {
-        return _state;
+        this.dispatch = _dispatch;
+        this.getState = _getState;
     }
 
     subscribe(cb) {
